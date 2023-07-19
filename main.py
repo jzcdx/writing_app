320b5c88fce2c2df3b4f511e3c2b8f92ed76ed9e
MicroOptimization <jackiezhen538@gmail.com>
2023-07-19 12:16:58 -0400

    first

---
+++ main.py
@@ -0,0 +1,19 @@
+import docx
+from os import walk
+
+def getFileNames(dir): #param: directory
+    filenames = next(walk(dir), (None, None, []))[2]  # [] if no file
+    return filenames
+
+def getWordCount(filepath):
+    doc = docx.Document(filepath)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return len("\n".join(fullText).split(" "))
+
+
+dir = "C:/Users/Codia/Desktop/royal_road_project/test_folder"
+print(getFileNames(dir))
+
+print(getWordCount("test.docx"))
