c7ce992f58387c479fa69a0d07fe87e8c7d2cfa6
MicroOptimization <jackiezhen538@gmail.com>
2023-07-21 10:50:30 -0400

    sums up all words of every word doc in a directory now

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import python-docx
+import docx
 from os import walk
 
 
@@ -7,19 +7,24 @@ def getFileNames(dir): #param: directory
     filenames = next(walk(dir), (None, None, []))[2]  # [] if no file
     return filenames
 
-def getWordCount(filepath):
-    doc = docx.Document(filepath)
+def getWordCount(filename):
+    doc = docx.Document(filename)
     fullText = []
     for para in doc.paragraphs:
         fullText.append(para.text)
     return len("\n".join(fullText).split(" "))
 
+def getTotalWordCount(file_names):
+    total_words = 0;
+    for i in file_names:
+        total_words += getWordCount(dir + "/" + i)
+    return total_words
 
 dir = "C:/Users/Codia/Desktop/royal_road_project/test_folder"
-file_names = getFileNames(dir);
 
-total_words = 0;
-for i in file_names:
-    total_words += getWordCount(i)
+file_names = getFileNames(dir);
+total_words = getTotalWordCount(file_names)
 
 print("total words: " , total_words)
+#print(getWordCount(dir + "/test2.docx"))
+
